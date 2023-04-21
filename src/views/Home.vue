5eb48077ac51d7a2f5ca2d69c68e02bf6f443c7f
Isreal6ix <isrealsix@gmail.com>
2023-04-22 00:15:33 +0700

    added methods and commputed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,43 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+
+const count = ref(0);
+
+const increaseCount = () => {
+	++count.value;
+};
+
+const decreaseCount = () => {
+	if (count.value <= 0) return;
+	--count.value;
+};
+
+const oddOrEven = computed(() => {
+	if (count.value % 2 === 0) return 'even';
+	return 'odd';
+});
+</script>
+
 <template>
 	<div class="home">
-		<h1>Home</h1>
+		<div class="count">{{ count }}</div>
+		<div class="buttons">
+			<button @click="increaseCount">+</button>
+			<button @click="decreaseCount">-</button>
+		</div>
+		<br />
+		<div>This counter is: {{ oddOrEven }}</div>
 	</div>
 </template>
+
+<style>
+.count {
+	font-size: 60px;
+	margin: 20px;
+}
+
+.buttons button {
+	font-size: 40px;
+	margin: 10px;
+}
+</style>
