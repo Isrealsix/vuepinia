34cd71a94be090be969636519df321c82393c4d2
Isreal6ix <isrealsix@gmail.com>
2023-04-24 08:33:17 +0700

    implemented sync-pinia-state-across-browser-tabs

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia';
 import piniPluginPersistedState from "pinia-plugin-persistedstate";
+import { PiniaSharedState } from 'pinia-shared-state';
 import { router } from './router/index';
 import App from './App.vue'
 import './style.css'
 
 const pinia = createPinia();
 pinia.use(piniPluginPersistedState);
+pinia.use(PiniaSharedState({
+	enable: true,
+	initialize: false,
+	type: 'localstorage'
+}));
 const app = createApp(App);
 app.use(router);
 app.use(pinia);
